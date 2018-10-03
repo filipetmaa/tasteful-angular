d9293bec1161398001aaf902a906abba1cc1f369
filipetmaa <filipetmaa@gmail.com>
2018-10-03 14:57:54 +0100

    todoapp 4

--- src/app/todo-app/todo-item/todo-item.component.ts
+++ src/app/todo-app/todo-item/todo-item.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+import { ToggleTodo } from '@state/todo-list/todo-list.actions';
+import { State, TodoItem } from '@state/todo-list/todo-list.state';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  todoList$: Observable<any>;
+    index: Number;
+    selectedItem: TodoItem;
 
-  ngOnInit() {
-  }
+    constructor(
+        private store: Store<State>,
+        private route: ActivatedRoute
+    ) {
+        this.route.params.subscribe(params => {
+            this.index = +params['index']; // (+) converts string 'index' to a number
+        });
+        this.todoList$ = store.pipe(select('todoList'));
+
+    }
+
+    ngOnInit() {
+    }
+
+    toggleTodo(index: Number) {
+        this.store.dispatch(
+            new ToggleTodo({ index })
+        );
+    }
 
 }
